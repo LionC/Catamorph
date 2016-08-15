fe61318e544f5f02a003b42d8ebde7a66b4bd34f
Leon Strauss <leon@ilu.st>
2016-08-15 11:19:05 +0200

    initial commit

---
+++ Assets/ButtonBehaviour.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonBehaviour : MonoBehaviour {
+
+	public GameObject toDestroy;
+	public Sprite afterTriggerSprite;
+
+	bool pressed = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void OnTriggerEnter2D() {
+		if (pressed == true)
+			return;
+
+		pressed = true;
+		GetComponent<SpriteRenderer> ().sprite = afterTriggerSprite;
+		Destroy (toDestroy);
+	}
+}
---
+++ Assets/Scripts/CatBehaviour.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+
+public class CatBehaviour : MonoBehaviour {
+
+	private enum Character {
+		FREEZE, FLAME
+	}
+
+	private Character character = Character.FREEZE;
+
+	private IDictionary colorMap = new Dictionary<Character, Color> ();
+
+	// Use this for initialization
+	void Start () {
+		colorMap.Add (Character.FREEZE, new Color (0, .2f, .8f));
+		colorMap.Add (Character.FLAME, new Color (.8f, .2f, 0));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Alpha1))
+			switchCharacter(Character.FLAME);
+				
+		if (Input.GetKeyDown (KeyCode.Alpha2))
+			switchCharacter(Character.FREEZE);
+	}
+
+	private bool switchCharacter(Character to) {
+		if (character == to)
+			return false;
+
+		character = to;
+		gameObject.GetComponent<SpriteRenderer> ().color = (Color)colorMap[to];
+
+		return true;
+	}
+}
+
