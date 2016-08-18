196cbef9074fc84429dc50f7e0172a51b06532cb
Milisa <m.jettkandt@gmx.de>
2016-08-18 10:04:00 +0200

    Timer added to GUI

---
+++ Assets/Scripts/TimerScript.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TimerScript : MonoBehaviour {
+	public Text text;
+
+
+	bool running = false;
+	float timePassed = 0;
+
+	void startClock(){
+		running = true;
+	
+	}
+
+	void stopClock(){
+		running = false;
+	}
+	// Use this for initialization
+	void Start () {
+		startClock ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!running)
+			return;
+
+		timePassed += Time.fixedDeltaTime;
+		
+		var minutes = timePassed / 60;  
+		var seconds = timePassed % 60;
+
+		text.text = string.Format ("{0:00}:{1:00}", minutes, seconds);
+	}
+}
