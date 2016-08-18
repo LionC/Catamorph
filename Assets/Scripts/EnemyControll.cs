6ad612bbb48180ea299234fe0c6b3a38ed805842
Jannis Hamborg <jahamborg@hotmail.de>
2016-08-18 15:52:58 +0200

    KatzenMinze BugFix, enemys improve

--- Assets/Scripts/EnemyControll.cs
+++ Assets/Scripts/EnemyControll.cs
@@ -6,7 +6,7 @@ public class EnemyControll : MonoBehaviour {
 	public GameObject player;
 	public bool hindernis, hit;
 	public Vector3 posEnemy, posPlayer;
-	public float abs, timeLeftHit, timeLeftJump, jumpTry;
+	public float abs, timeLeftHit, timeLeftJump, timeBug, jumpTry;
 	public float damageValue = 1f;
 
 	// Use this for initialization
@@ -36,18 +36,19 @@ public class EnemyControll : MonoBehaviour {
 		abs = Vector3.Distance (posEnemy, posPlayer);
 
 		if (abs < 3) {
-			transform.position += new Vector3 (((posPlayer.x - posEnemy.x) / 10), 0.0f, 0.0f);
-			if ((posPlayer.y - posEnemy.y > 0.5f || hindernis == true) && timeLeftJump <= 0) {
-				jumpTry += 0.2f;
-				transform.position += new Vector3 (0.0f, jumpTry, 0.0f);
-				timeLeftJump = 0.2f;
-			}
-			if (posEnemy.y - posPlayer.y > 0) {
-				transform.position += new Vector3 (player.GetComponent<Rigidbody2D>().velocity.x, jumpTry, 0.0f);
-			}
-			hindernis = false;
+			transform.position += new Vector3 (((posPlayer.x - posEnemy.x) / (10*Random.Range(0,2))), 0.0f, 0.0f);
+				if ((posPlayer.y - posEnemy.y > 0.5f || hindernis == true) && timeLeftJump <= 0) {
+					jumpTry += 0.2f;
+					transform.position += new Vector3 (0.0f, jumpTry, 0.0f);
+					timeLeftJump = 0.2f;
+				}
+				if (posEnemy.y - posPlayer.y > 0) {
+					transform.position += new Vector3 (player.GetComponent<Rigidbody2D> ().velocity.x, jumpTry, 0.0f);
+				}
+				hindernis = false;
+			} 
 		}
-	}
+
 
 		private void OnCollisionEnter2D(Collision2D coll){
 		if ((tag == "Dog") || (tag == "Human")){
