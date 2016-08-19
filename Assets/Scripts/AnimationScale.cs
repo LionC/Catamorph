fd9e6d898273271d85d80d3d65344e69b387285f
Jannis Hamborg <jahamborg@hotmail.de>
2016-08-19 14:55:34 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	Assets/Janis.unity

---
+++ Assets/Scripts/AnimationScale.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimationScale : MonoBehaviour {
+	public Sprite sprite;
+
+	// Use this for initialization
+	void Start () {
+		GetComponent (SpriteRenderer).sprite;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
