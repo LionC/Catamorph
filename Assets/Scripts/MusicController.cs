274d15c0310d3b48af29e58181ed4743a47c417a
Leon Vack <LVack@vi-solutions.de>
2016-08-19 16:09:51 +0200

    added some music and effect sounds
    * added some music track files
    * added some sound effect files
    * created MusicController script (plays a given list of tracks in random
    order, adds a filter on cat mint contact)
    * create SoundOnCollide script (plays a given sound on contact based on
    the ohter objects tag)

---
+++ Assets/Scripts/MusicController.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Audio;
+using UnityStandardAssets._2D;
+
+public class MusicController : MonoBehaviour { 
+
+    public AudioMixerSnapshot defaultAudio;
+    public AudioMixerSnapshot invertedAudio;
+    public AudioClip[] tracks;
+    public AudioSource audioSource;
+
+    public GameObject player;
+
+    public float invertedTransisionIn = 1f;
+    public float invertedTransisionOut = 1f;
+
+    private bool isMusicInverted;
+    private PlatformerCharacter2D character;
+
+
+    // Use this for initialization
+    void Start () {
+        defaultAudio.TransitionTo(0);
+        character = player.GetComponent<PlatformerCharacter2D>();
+        playRandomSong();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        // if charecter movement and inverted music have different values, change inverted music
+        if (character.inverted ^ isMusicInverted) setInvertedMusic(!isMusicInverted);
+        if (!audioSource.isPlaying) playRandomSong();
+	}
+
+    void setInvertedMusic (bool state) {
+        isMusicInverted = state;
+        if (state) invertedAudio.TransitionTo(invertedTransisionIn);
+        else defaultAudio.TransitionTo(invertedTransisionOut);
+    }
+
+    void playRandomSong() {
+        int song = Random.Range(0, tracks.Length);
+        audioSource.clip = tracks[song];
+        audioSource.Play();
+    }
+}
---
+++ Assets/Scripts/SoundOnCollide.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+using UnityStandardAssets._2D;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+
+public class SoundOnCollide : MonoBehaviour {
+    private static readonly long cdToTicks = TimeSpan.FromSeconds(1).Ticks;
+
+    public bool disable;
+    public float cooldownSeconds;
+    public AudioSource audioSource;
+    public List<string> targetTags;
+    public List<AudioClip> enterSounds;
+    public List<AudioClip> exitSounds;
+
+    private long lastStart;
+
+    // Use this for initialization
+    void Start () {
+        lastStart = 0;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter2D(Collider2D other) {
+        AudioClip clip = GetAudioClipForTag(enterSounds, other.tag);
+        if (clip != null) playClip(clip);
+    }
+
+    void OnTriggerExit2D(Collider2D other) {
+        AudioClip clip = GetAudioClipForTag(exitSounds, other.tag);
+        if (clip != null) playClip(clip);
+    }
+
+    private bool playClip(AudioClip clip) {
+        if (clip == null) throw new ArgumentNullException("Clip must not be null!");
+
+        long now = DateTime.Now.Ticks;
+        if (now - lastStart < cooldownSeconds * cdToTicks) return false;
+
+        if (audioSource.isPlaying) audioSource.Stop();
+        audioSource.clip = clip;
+        audioSource.Play();
+        lastStart = now;
+        return true;
+    }
+
+    private int GetTagId(string tag) {
+        return targetTags.IndexOf(tag);
+    }
+
+    private AudioClip GetAudioClipForTag(List<AudioClip> clips, string tag) {
+        return targetTags.Contains(tag) ? clips[GetTagId(tag)] : null;
+    }
+
+    void setDisable(bool disable) {
+        audioSource.mute = disable;
+        this.disable = disable;
+    }
+}
