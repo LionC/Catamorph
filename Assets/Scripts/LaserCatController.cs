73220c5284f6b9f5f72ed5d84e5b6c2ef80c4af4
Fabrice <fabrice.vonderlehr@gmail.com>
2016-08-19 14:41:40 +0200

    edited laserCat

--- Assets/Scripts/LaserCatController.cs
+++ Assets/Scripts/LaserCatController.cs
@@ -3,13 +3,22 @@ using System.Collections;
 
 public class LaserCatController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
+	public GameObject player;
+	public Color laserCatColor = new Color(220, 20, 60);
+
 	// Update is called once per frame
 	void Update () {
-	
+
+	}
+
+	void OnEnable() {
+		player.GetComponent<SpriteRenderer> ().color = laserCatColor; 
+	}
+
+	void OnDisable() {
+	}
+
+	public override string ToString() {
+		return "LaserCat";
 	}
 }
