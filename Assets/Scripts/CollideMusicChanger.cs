c6c836242a03288588bdde0d1039c7f997561d48
Leon Vack <LVack@vi-solutions.de>
2016-08-21 23:08:38 +0200

    added comments to music sound scripts
    * added comments to CollideMusicChanger, MusicController and
    SoundOnCollider script

--- Assets/Scripts/CollideMusicChanger.cs
+++ Assets/Scripts/CollideMusicChanger.cs
@@ -4,15 +4,21 @@ using System.Collections.Generic;
 
 public class CollideMusicChanger : MonoBehaviour {
 
+    // list of tags to change music
     public List<string> tags;
+    // music type to change to during trigger
     public MusicController.MusicType targetType;
+    // main music controller music change commands are sent to
     public MusicController controller;
 
+    // variable to keep track of music type to return to
     private MusicController.MusicType previousType;
+    // flag to keep track whether trigger is active or not
     private bool triggered;
 
 	// Use this for initialization
 	void Start () {
+        // initalize triggered with false and previousType with the music type currently playing
         triggered = false;
         previousType = controller.currentMusicType;
     }
@@ -23,20 +29,24 @@ public class CollideMusicChanger : MonoBehaviour {
 	}
 
     void OnTriggerEnter2D(Collider2D other) {
-        Debug.Log("enter tag:" + other.tag);
+        // ensure triggred is true and the tag is valid
         if (triggered || !VaildTag(other.tag)) return;
 
+        // set triggered
         triggered = true;
+        // store music type currently playing
         previousType = controller.currentMusicType;
-        Debug.Log("changing music from " + previousType + " to " + targetType);
+        // make controller change the music type to the target type
         controller.TransitionToMusicType(targetType);
     }
 
     void OnTriggerExit2D(Collider2D other) {
-        Debug.Log("exit tag:" + other.tag);
+        // ensure triggred is false and the tag is valid
         if (!triggered || !VaildTag(other.tag)) return;
+
+        // clear triggerd
         triggered = false;
-        Debug.Log("changing music back from " + targetType + " to " + previousType);
+        // make controller change the music type to the previouse type
         controller.TransitionToMusicType(previousType);
     }
 
