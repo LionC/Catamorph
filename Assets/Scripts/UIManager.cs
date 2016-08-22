e51c10d6532b0228d0e0b8a1566aa05ceab323bf
Tobias Chen <tobias@chen.de>
2016-08-22 13:43:53 +0200

    added new scene MainMenu

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+
+public class UIManager : MonoBehaviour {
+	public void Quit()
+	{
+		Application.Quit();
+	}
+
+}
