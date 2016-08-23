a3f7235b082a4afcc76abb8eb6701985175bf993
Hannes <hannes@guedelhoefer.de>
2016-08-23 16:15:18 +0200

    added a nyan'tastisches secret

---
+++ Assets/NyanCatCollider.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NyanCatCollider : MonoBehaviour {
+
+
+
+	public NyanCat nyanCat;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		nyanCat.play = true;
+	}
+}
