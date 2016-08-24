1043020a0f4df84e2db062d723ceba9efb4702f2
hsakagamedev <hsaka@ilu.st>
2016-08-25 00:57:29 +0200

    Explosion on shot working, not working on jump

--- Assets/Scripts/RocketController.cs
+++ Assets/Scripts/RocketController.cs
@@ -5,13 +5,20 @@ public class RocketController : MonoBehaviour {
 
 	public float damage = 1.0f;  //Damage caused by Rocket
 	public float livingTime = 20f;  //Time in sec, at which the rocket is destroyed at the latest
+	private Animator animator;
 
 		
 	void Start () {
-		Destroy (gameObject, 20);  //Destroy rocket at the latest
+		animator = GetComponent<Animator>();
+		DestroyRocket(20);  //Destroy rocket at the latest
 	}
 		
 	void OnCollisionEnter2D(Collision2D other) {
-		Destroy (gameObject);  //Destroy rocket on collision
+		animator.SetTrigger("Explode");
+		DestroyRocket(0);  //Destroy rocket on collision
+	}
+	
+	void DestroyRocket(float timeOut) {
+		Destroy (gameObject, animator.GetCurrentAnimatorStateInfo(0).length + timeOut);
 	}
 }
