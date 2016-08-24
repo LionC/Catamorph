27f554eecfca3cef01ed2bff601a1d61eda6ffa3
hsakagamedev <hsaka@ilu.st>
2016-08-24 15:34:42 +0200

    emp implemented

---
+++ Assets/Scripts/EmpAnimationRenderer.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class EmpAnimationRenderer : MonoBehaviour {
+
+	private EmpController empController;
+
+	void Start() {
+		empController = GameObject.FindGameObjectWithTag("EMP").GetComponent<EmpController> ();
+		gameObject.GetComponent<Image> ().color = new Color32 (255,255,255,0);
+	}
+
+	void FixedUpdate () {		
+		if (empController.hitCatEmp()) {
+			GetComponent<Image> ().sprite = GetComponent<SpriteRenderer> ().sprite;
+			gameObject.GetComponent<Image> ().color = new Color32 (255,255,255,255);
+		} 
+		else
+			gameObject.GetComponent<Image> ().color = new Color32 (255,255,255,0);
+	}
+}
---
+++ Assets/Scripts/EmpBackgroundRenderer.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class EmpBackgroundRenderer : MonoBehaviour {
+
+	private EmpController empController;
+
+	void Start() {
+		empController = GameObject.FindGameObjectWithTag("EMP").GetComponent<EmpController> ();
+		gameObject.GetComponent<Image> ().color = new Color32 (0,0,0,0);
+	}
+
+	void FixedUpdate () {		
+		if (empController.hitCatEmp())
+			gameObject.GetComponent<Image> ().color = new Color32 (0,0,0, 180);
+		else
+			gameObject.GetComponent<Image> ().color = new Color32 (0,0,0,0);
+	}
+}
