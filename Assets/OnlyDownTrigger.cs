4a0c1904e6346a393940d296c6364bf251e2cce4
Hannes <hannes@guedelhoefer.de>
2016-08-24 10:57:33 +0200

    fixed a cheeting strat in tut3

---
+++ Assets/OnlyDownTrigger.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OnlyDownTrigger : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		if (other.tag == "Player") {
+			var vel = other.GetComponent<Rigidbody2D> ().velocity;
+
+			if (vel.y > 0) {
+				vel.y = -vel.y;
+				other.GetComponent<Rigidbody2D> ().velocity = vel;
+			}
+		}
+	}
+}
